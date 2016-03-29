0b4b625977c5887e0f97991b5af2cac33b42ba7d
Dario <dario.guida@my.westminster.ac.uk>
2016-03-29 10:01:58 +0100

    Add New Event Button

---
+++ src/main/java/com/darioguida/calendarapp/CalendarActivity.java
@@ -0,0 +1,35 @@
+package com.darioguida.calendarapp;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CalendarView;
+
+
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_view);
+
+        CalendarView calendar=(CalendarView)findViewById(R.id.calendarView);
+        //--Button newEvent=(Button)findViewById(R.id.newEvent);
+        if (calendar != null) {
+            calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+                @Override
+                public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                    Log.d("Test-------", "----Test-----");
+                    System.out.println(view.getDate());
+                }
+            });
+        }
+
+
+    }
+
+
+
+}
